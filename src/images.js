fec279e01dc698ee03e61da5dd0237eef21f9f43
Amanda Dovel <amandacdovel@gmail.com>
2019-07-25 12:50:39 -0600

    finish images

---
+++ src/images.js
@@ -0,0 +1,30 @@
+import bart from './images/bart.png';
+import chiefWiggam from './images/chief-wiggam.gif';
+import homerSimpson from './images/homer-simposon.png'
+import krustyTheKlown from './images/krusty-the-klown.png';
+import maggie from './images/maggie.jpg';
+import milhouse from './images/milhouse.png';
+import mrBurns from './images/mr-burns.png';
+import nedFlanders from './images/ned-flanders.jpg';
+import nelson from './images/nelson';
+import ralph from './images/ralph.jpg';
+import santasLilHelper from './images/santas-lil-helper.png';
+import sideshowBob from './images/sideshow-bob.png';
+
+const images = [
+    bart, 
+    chiefWiggam, 
+    homerSimpson, 
+    krustyTheKlown, 
+    maggie,  
+    milhouse,  
+    mrBurns,  
+    nedFlanders,  
+    nelson,  
+    ralph,  
+    santasLilHelper,  
+    sideshowBob  
+]
+
+export default images;
+
