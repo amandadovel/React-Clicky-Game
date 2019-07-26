bff7102a80505314700e1fa92df1c835113c0cfe
Amanda Dovel <amandacdovel@gmail.com>
2019-07-25 23:25:12 -0600

    Updates

---
+++ precache-manifest.95b87c1d7c99c065bf2fba69b88727d4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0981e52acd73c165c331fb8d9f299c20",
+    "url": "/React-Clicky-Game/index.html"
+  },
+  {
+    "revision": "f91785d34cbedcec32dd",
+    "url": "/React-Clicky-Game/static/css/main.a1763f36.chunk.css"
+  },
+  {
+    "revision": "35eac6df1058b95f4850",
+    "url": "/React-Clicky-Game/static/js/2.ef8084ba.chunk.js"
+  },
+  {
+    "revision": "f91785d34cbedcec32dd",
+    "url": "/React-Clicky-Game/static/js/main.4ca5a20c.chunk.js"
+  },
+  {
+    "revision": "339ef2b4a9d9b3bbcf8d",
+    "url": "/React-Clicky-Game/static/js/runtime~main.25e3a6ff.js"
+  },
+  {
+    "revision": "7aa0f2f27ef0df664d0f23488f144ba4",
+    "url": "/React-Clicky-Game/static/media/bart.7aa0f2f2.png"
+  },
+  {
+    "revision": "d5d76f07a5c1b3c77a9b9e0dfe852a5d",
+    "url": "/React-Clicky-Game/static/media/chief-wiggam.d5d76f07.gif"
+  },
+  {
+    "revision": "91a7e1fa36dac6e5d2c4368951d8c166",
+    "url": "/React-Clicky-Game/static/media/clouds.91a7e1fa.jpg"
+  },
+  {
+    "revision": "82350cd6abc629b4b8a966f7321ce5d1",
+    "url": "/React-Clicky-Game/static/media/homer.82350cd6.jpg"
+  },
+  {
+    "revision": "84ff8b75ecf3d7bbfb61b2ac6606658f",
+    "url": "/React-Clicky-Game/static/media/krusty.84ff8b75.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Clicky-Game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d825e64115b50cc20ba74f9a8c5876c9",
+    "url": "/React-Clicky-Game/static/media/maggie.d825e641.jpg"
+  },
+  {
+    "revision": "e9ec71ea26881a231394a14e77a50e73",
+    "url": "/React-Clicky-Game/static/media/marge.e9ec71ea.jpg"
+  },
+  {
+    "revision": "d6f5c679a94bb7b67e3d4c596e47d45a",
+    "url": "/React-Clicky-Game/static/media/milhouse.d6f5c679.png"
+  },
+  {
+    "revision": "a60c0247c059ba1a8c119a432e30195b",
+    "url": "/React-Clicky-Game/static/media/mr-burns.a60c0247.png"
+  },
+  {
+    "revision": "90decb36d07e1f9334fa0e031d0d3505",
+    "url": "/React-Clicky-Game/static/media/ned-flanders.90decb36.jpg"
+  },
+  {
+    "revision": "2f0e66c12d130d9cfea8d95d693d06fb",
+    "url": "/React-Clicky-Game/static/media/ralph.2f0e66c1.jpg"
+  },
+  {
+    "revision": "ebbeb55f6e425f75148e49640c29e34b",
+    "url": "/React-Clicky-Game/static/media/santas-lil-helper.ebbeb55f.jpg"
+  },
+  {
+    "revision": "f4fc2946803961bb8155fb0c1e13def9",
+    "url": "/React-Clicky-Game/static/media/sideshow-bob.f4fc2946.png"
+  },
+  {
+    "revision": "00e432d1d992f208440dcf1da7af9d52",
+    "url": "/React-Clicky-Game/static/media/simpsons-couch.00e432d1.jpg"
+  }
+]);
